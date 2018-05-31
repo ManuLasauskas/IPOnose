bc4554139a9ea502ad671a1c38199aafae09c932
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-05-31 16:11:13 +0200

    El logo

--- IPOv1/src/Presentacion/UserChat.java
+++ IPOv1/src/Presentacion/UserChat.java
@@ -1,12 +1,72 @@
 package Presentacion;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import Dominio.Usuario;
+import Persistencia.Agente;
+import net.miginfocom.swing.MigLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JComboBox;
 
 public class UserChat extends JPanel {
-
+	private JTextField textField;
 	
 	public UserChat() {
-
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{32, 251, 138, 0, 0};
+		gridBagLayout.rowHeights = new int[]{66, 146, 51, 32, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 2;
+		gbc_comboBox.gridy = 0;
+		add(comboBox, gbc_comboBox);
+		
+		JTextArea textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridwidth = 2;
+		gbc_textArea.insets = new Insets(0, 0, 5, 5);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 1;
+		gbc_textArea.gridy = 1;
+		add(textArea, gbc_textArea);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 2;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 2;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnEnviar = new JButton("Enviar");
+		GridBagConstraints gbc_btnEnviar = new GridBagConstraints();
+		gbc_btnEnviar.gridwidth = 2;
+		gbc_btnEnviar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnEnviar.gridx = 1;
+		gbc_btnEnviar.gridy = 3;
+		add(btnEnviar, gbc_btnEnviar);
+		
 	}
 
 }
